6f12982621c597a261adcaf757e9589d9a8b805d
Viet Bui <anthonybui311205@gmail.com>
2025-08-13 12:19:49 +0700

    Make change to gitignore, adding .env in, refactor folder

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,7 @@
-from backend import audio_extractor 
-from backend import speech_to_text
-from backend import text_translation
-from backend.utils.audio_reattached import process_video_with_dub
+from backend.translation import audio_extractor 
+from backend.translation import speech_to_text
+from backend.translation import text_translation
+from backend.translation.utils.audio_reattached import process_video_with_dub
 
 """
     The function lang_convert() is used to convert the language name to its respective language code.
